234cab80d74d361d351d38951978a4d340372fa3
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-03-01 17:15:26 -0700

    Update higher-order.js

--- higher-order.js
+++ higher-order.js
@@ -20,8 +20,13 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+// function evenizer (num) {
+//   if (num%2 === 0) {
+//     return num;
+//   }
+// }
+
+let evenNumbers = mixedNumbers.filter(num => num % 2 === 0);
 
 
 
@@ -43,9 +48,11 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+// function tax(price) {
+//   return price * 1.07
+// }
 
+let postTaxPrices = prices.map(x=>x*1.07);
 
 
 ////////// PROBLEM 3 //////////
@@ -62,8 +69,11 @@ const populations = [8175133, 3792621, 2695598, 2100263];
   (runningTotal, curElement, curIndex, wholeArray)=>{} Arrow Form
 */
 
-//Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+function sum (total, pop) {
+  return total + pop;
+}
+
+let totalPopulation = populations.reduce(sum)
 
 
 
@@ -88,8 +98,14 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
   Use the filter method to return only the monsters that have a CP of over 200.
 */
 
-//Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+function cb() {
+  if (monstersInYourPocket[prop] < 200) {
+    delete monstersInYourPocket[prop]
+  }
+  return monstersInYourPocket;
+}
+
+let myStrongest = monstersInYourPocket.filter(cb)
 
 
 
